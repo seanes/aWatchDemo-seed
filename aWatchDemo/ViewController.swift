fb783446f921005a42f2e2d5a226f32ea56637d2
Sean Erik Scully <seanscully@seans-macbook-pro.local>
2016-06-08 15:27:24 +0200

    Initial Commit

---
+++ aWatchDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aWatchDemo
+//
+//  Created by Sean Scully on 08/06/16.
+//  Copyright © 2016 Sean Scully. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
