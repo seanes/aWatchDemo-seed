b86b2a765cb4e8e5e7bfa7b5ad8dccd7a67e8e8f
Terje Lønøy <terje.loenoey@norwegian.com>
2016-08-22 16:17:17 +0200

    Added remote notifications

--- aWatchDemoWK Extension/NotificationController.swift
+++ aWatchDemoWK Extension/NotificationController.swift
@@ -31,6 +31,27 @@ class NotificationController: WKUserNotificationInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+
+    
+    override func didReceiveLocalNotification(localNotification: UILocalNotification, withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
+        // This method is called when a local notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+        //
+        // After populating your dynamic notification interface call the completion block.
+        
+        if (localNotification.alertTitle == "NICKELBACK!") {
+            label.setText("Nickelback! :)")
+            notificationImage.setImage(UIImage.init(named: "nickelback_image"))
+            
+        } else {
+            label.setText("Kroeger! :)")
+            notificationImage.setImageNamed("kroeger_image")
+        }
+        
+        completionHandler(.Custom)
+    }
+    
     
     override func didReceiveRemoteNotification(remoteNotification: [NSObject : AnyObject], withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
         // This method is called when a remote notification needs to be presented.
