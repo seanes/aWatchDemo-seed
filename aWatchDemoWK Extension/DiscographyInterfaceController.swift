e6bf3b1c480eb455473b7918df41ccd35a403d6d
Sean Scully <sean.erik.scully@gmail.com>
2016-08-22 07:45:53 +0200

    clearified comments, not made things so hard for us

---
+++ aWatchDemoWK Extension/DiscographyInterfaceController.swift
@@ -0,0 +1,9 @@
+//
+//  DiscographyInterfaceController.swift
+//  aWatchDemo
+//
+//  Created by Sean Scully on 21/08/16.
+//  Copyright © 2016 Sean Scully. All rights reserved.
+//
+
+import Foundation
